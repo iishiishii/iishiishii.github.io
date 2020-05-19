811550bdc703401e6dbda36f7d4c00938e583d59
Lunatic Luna <59903692+iishiishii@users.noreply.github.com>
2020-05-19 12:00:14 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+const navSlide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav_links");
+	const navLinks = document.querySelectorAll(".nav_links li");
+
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active"); //toggle nav
+
+		navLinks.forEach((link, index) => { //animate links
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index/8 + 0.7}s`;
+			}
+		});
+
+		burger.classList.toggle('toggle');
+	});
+
+}
+navSlide();
